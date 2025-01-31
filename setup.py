9342e8eba233570f6d97650d5ffff850eb92bc95
SamanHmd <SamanHamidishal@gmail.com>
2025-01-30 21:45:00 -0500

    fixed encoding problem

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 setup(
@@ -12,7 +13,7 @@ setup(
     author="Sam",
     author_email="SamanHamidishal@gmail.com",
     description="A Python package to fetch PM2.5 air quality data using the AQICN API.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/samanhmd/aqi_monitor",
     classifiers=[
